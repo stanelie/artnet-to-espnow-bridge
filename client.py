91b28134ff79a59ad9990d4a4774f691a2ca36df
stanelie <github@stanelie.com>
2024-11-19 10:58:40 -0500

    catch invalid buffer and restart espnow

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 # Circuitpython on Waveshare ESP32-S3-ZERO
+# artnet espnow client/pixel
 
 import espnow
 import wifi
 import board
 import time
 import neopixel
+import microcontroller
 
 channel = 1
 rcvd_channel = 0
-start_time = 0
 dmx_data = bytearray(513)
-packet_received = 0
+packet_received_flag = 0
 
 wifi.radio.start_ap(" ", "", channel=channel, max_connections=10)
 wifi.radio.stop_ap()
@@ -21,10 +22,16 @@ def mac2Str(wifi_mac):
 
 print(f"MAC: {wifi_mac} -> {mac2Str(wifi_mac)}")
 
-
 print(f"scan starting on channel {channel}...")
 
-e = espnow.ESPNow()
+peer = espnow.Peer(b'\xff\xff\xff\xff\xff\xff')
+
+def start_espnow():
+    global e
+    e = espnow.ESPNow()
+    e.peers.append(peer)
+    
+start_espnow()
 
 pixel_pin = board.NEOPIXEL
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -36,10 +43,10 @@ def start_ap(channel):
     wifi.radio.stop_ap()
     
 def start_ap_once_more(channel):
-    global packet_received
-    if packet_received == 0:
+    global packet_received_flag
+    if packet_received_flag == 0:
         print(f"got an ESPNow packet that says we're really on channel {channel}")
-        packet_received = 1
+        packet_received_flag = 1
         start_ap(channel)
 
 def check_for_packet():
@@ -51,25 +58,39 @@ def update_pixels(dmx_data):
     dmx2 = dmx_data[2]
     dmx3 = dmx_data[3]
     pixels.fill((dmx2,dmx1,dmx3))
+packet = 0
+def read_packet():
+    global packet
+    try:
+        packet = e.read()
+    except ValueError as error:
+        print(f"{error}")
+        time.sleep(0.1)
+#        microcontroller.reset()
+        e.deinit()
+        start_espnow()
+        
+old_packet = 0
 
 while True:
-    
-    start_time = time.monotonic()
-    while time.monotonic() - start_time < 0.6:
-        if check_for_packet():
-            try:
-                packet = e.read()               
-                dmx_data = packet[1]
-                channel = dmx_data[0]
-                update_pixels(dmx_data)
-                start_ap_once_more(channel)
-            except:
-                pass
-            break
+    if check_for_packet():
+        read_packet()
+        if packet[1] != old_packet:
+            dmx_data = packet[1]
+            channel = dmx_data[0]
+            update_pixels(dmx_data)
+            start_ap_once_more(channel)
+#            e.send(dmx_data,peer)
+            old_packet = packet[1]
+#            print(f"{packet[0]}")
+
     else:
-        if packet_received == 0: # No packet received, restart AP and increment channel
+        if packet_received_flag == 0: # No packet received, restart AP and increment channel
             channel = (channel % 11) + 1
             pixels.fill((0,5,0))
             time.sleep(0.05)
             pixels.fill((0,0,0))
             start_ap(channel)
+            time.sleep(0.3)
+
+
