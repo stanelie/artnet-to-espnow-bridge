8d0fac4e1c833434cedf7a1da1bf01e57b8dba04
stanelie <github@stanelie.com>
2024-11-29 13:29:02 -0500

    Create config.py

---
+++ bridge/config.py
@@ -0,0 +1,6 @@
+# configuration du bridge artnet-ESPNow
+
+SSID = "sticks"      # nom du réseau wifi
+PASSWORD = "patateaufour"      # clé réseau wifi
+STARTDMX = 1                 # offset du premier canal DMX utilisé. Exemple : offset 0, le premier canal sera 1
+UNIVERSE = 1
