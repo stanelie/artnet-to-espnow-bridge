8a30cf89490abc3ab53ba4f0f030df796200335b
stanelie <github@stanelie.com>
2024-11-29 13:32:48 -0500

    Update code.py

--- client/code.py
+++ client/code.py
@@ -1,41 +1,29 @@
 # Circuitpython on Waveshare ESP32-S3-ZERO
-# artnet espnow client/repeater (see below)
+# artnet espnow client/pixel/repeater
+
 import espnow
 import wifi
 import board
 import time
 import neopixel
-import microcontroller
+import pwmio
 
-channel = 1
+# variables initialization
+is_repeater = False # change to True to make this device into a repeater
+channel = 1 # scan start channel, adjust to the correct channel to make it connect faster
 rcvd_channel = 0
 dmx_data = bytearray(513)
 packet_received_flag = 0
-
-wifi.radio.start_ap(" ", "", channel=channel, max_connections=10)
-wifi.radio.stop_ap()
-wifi_mac = wifi.radio.mac_address
-
-def mac2Str(wifi_mac): 
-    return ':'.join([f"{b:02X}" for b in wifi_mac])
-
-print(f"MAC: {wifi_mac} -> {mac2Str(wifi_mac)}")
-
-print(f"scan starting on channel {channel}...")
-
 peer = espnow.Peer(b'\xff\xff\xff\xff\xff\xff')
+errors = 1 # used to display error count
+packet = 0
+# old_packet = 0
 
 def start_espnow():
     global e
     e = espnow.ESPNow()
     e.peers.append(peer)
     
-start_espnow()
-
-pixel_pin = board.NEOPIXEL
-pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pixels.fill((0, 0, 0))
-
 def start_ap(channel):
     print(f"going to channel {channel}")
     wifi.radio.start_ap(b'my_ap', b'password', channel=channel)
@@ -44,50 +32,63 @@ def start_ap(channel):
 def start_ap_once_more(channel):
     global packet_received_flag
     if packet_received_flag == 0:
-        print(f"got an ESPNow packet that says we're really on channel {channel}")
+        print(f"got an ESPNow packet that says we're on channel {channel}")
         packet_received_flag = 1
         start_ap(channel)
+        onboard_pixel.fill((0,5,0)) # onboard LED stays green when correct channel has been reached
 
 def check_for_packet():
     if e:
         return True
-    
+
 def update_pixels(dmx_data):
     dmx1 = dmx_data[1]
     dmx2 = dmx_data[2]
     dmx3 = dmx_data[3]
-    pixels.fill((dmx2,dmx1,dmx3))
-packet = 0
+    dmx4 = dmx_data[4]
+    pixels.fill((dmx1,dmx2,dmx3,dmx4))
+    pwm.duty_cycle = 65534-(dmx1*255)
+    
 def read_packet():
     global packet
+    global errors
     try:
         packet = e.read()
     except ValueError as error:
-        print(f"{error}")
-        time.sleep(0.1)
-#        microcontroller.reset()
+        print(f"{error} error {errors}")
         e.deinit()
         start_espnow()
+        errors = errors +1
         
-old_packet = 0
+print("starting scan")
+start_ap(channel)
 
+onboard_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, pixel_order=neopixel.RGB) # onboard neopixel
+pixels = neopixel.NeoPixel(board.D1, 1,pixel_order=neopixel.RGBW) # big neopixel connected on digital output 1
+pixels.brightness = 1
+pixels.fill((0, 0, 0, 0))
+pwm = pwmio.PWMOut(board.D7, frequency=1000)
+start_espnow()
+        
 while True:
+
     if check_for_packet():
         read_packet()
-        if packet[1] != old_packet:
-            dmx_data = packet[1]
-            channel = dmx_data[0]
-            update_pixels(dmx_data)
+        dmx_data = packet[1]
+        channel = dmx_data[0]
+        update_pixels(dmx_data)
+        if not packet_received_flag: 
             start_ap_once_more(channel)
-#            e.send(dmx_data,peer)        ####### uncomment to make this into a repeater
-            old_packet = packet[1]
-#            print(f"{packet[0]}")
+        if is_repeater:
+            e.send(dmx_data,peer)
 
     else:
         if packet_received_flag == 0: # No packet received, restart AP and increment channel
             channel = (channel % 11) + 1
-            pixels.fill((0,5,0))
+            onboard_pixel.fill((5,0,0)) # blink red each time we switch channel
             time.sleep(0.05)
-            pixels.fill((0,0,0))
+            onboard_pixel.fill((0,0,0))
             start_ap(channel)
             time.sleep(0.3)
+
+ 
