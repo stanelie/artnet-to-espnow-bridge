8c636eb1973c35090ebf436c4c40e37037e6904a
stanelie <github@stanelie.com>
2024-12-03 15:32:03 -0500

    Update config.py

--- bridge/config.py
+++ bridge/config.py
@@ -4,3 +4,6 @@ SSID = "sticks"      # nom du réseau wifi
 PASSWORD = "patateaufour"      # clé réseau wifi
 STARTDMX = 1                 # offset du premier canal DMX utilisé. Exemple : offset 0, le premier canal sera 1
 UNIVERSE = 1
+USE_STATIC_IP = False # ignored if CREATE_AP = True
+CREATE_AP = False
+HOSTNAME = "lonestar-bridge"
