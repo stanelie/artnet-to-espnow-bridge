a6877096ab1f556e03bba82f6b8e0a1d764e6354
stanelie <github@stanelie.com>
2024-12-03 17:20:30 -0500

    format artpollreply correctly, wifi reconnect

--- bridge/code.py
+++ bridge/code.py
@@ -38,21 +38,20 @@ def connect_to_wifi():
         if USE_STATIC_IP:
             wifi.radio.set_ipv4_address(ipv4 = ipv4, netmask = netmask, gateway = gateway) # use static IP address
         wifi.radio.connect(SSID,PASSWORD, timeout=5)
+        time.sleep(1)
         global ip_address_str
         ip_address_str = str(wifi.radio.ipv4_address)
         global wifi_channel
         wifi_channel = wifi.radio.ap_info.channel
     except ConnectionError as e:
         print(f"{e} . Retrying...")
-        wifi.radio.stop_station()
         time.sleep(1)
-        connect_to_wifi()
         
 def create_wifi_AP():
     print(f"setting up wifi access point \"{SSID}\"...")
     CHANNEL = 1
     wifi.radio.hostname = HOSTNAME
-    wifi.radio.set_ipv4_address_ap(ipv4 = ipv4, netmask = netmask, gateway = gateway) # use static IP address
+    wifi.radio.set_ipv4_address_ap(ipv4 = ipv4, netmask = netmask, gateway = gateway)
     wifi.radio.start_ap(SSID, PASSWORD, channel=CHANNEL, max_connections=4)
     time.sleep(0.01) # bug : needed so IP is available in ip_address_str
     global ip_address_str
@@ -60,14 +59,7 @@ def create_wifi_AP():
     global wifi_channel
     wifi_channel = CHANNEL
 
-if not CREATE_AP:
-    connect_to_wifi() # connect to access point as wifi client
-else:
-    create_wifi_AP() # creates wifi access point
-
-hostname_bytes = wifi.radio.hostname.encode('utf-8')
-
-print(f"UP! my IP addr: {ip_address_str}, channel {wifi_channel}" )
+hostname_bytes = HOSTNAME.encode('utf-8')
 
 # setup LoRa radio
 RADIO_FREQ_MHZ = 433.0
@@ -80,12 +72,6 @@ rfm9x.spreading_factor = 7
 rfm9x.signal_bandwidth = 250000
 rfm9x.coding_rate = 5
 
-# massage ipaddress
-octets = ip_address_str.split('.')
-ip_address_bytes = bytearray(4)
-for i in range(4):
-    ip_address_bytes[i] = int(octets[i])
-    
 pixel_pin = board.NEOPIXEL
 # ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
@@ -105,7 +91,7 @@ last_sent_time = time.monotonic()
 reply_array = [
                 0x41, 0x72, 0x74, 0x2d, 0x4e, 0x65, 0x74, 0x00, # "Art-Net\0" header
                 0x00, 0x21,                                     # opcode
-                0x02, 0xe7, 0x14, 0x24,                         # our IP address
+                0x02, 0x00, 0x00, 0x01,                         # our IP address
                 0x36, 0x19,                                     # port 6454
                 0x00, 0x01,                                     # version info of this node
                 0x00,                                           # NetSwitch
@@ -132,19 +118,23 @@ reply_array = [
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                 ]
-# IP address
-reply_array[10] = ip_address_bytes[0]
-reply_array[11] = ip_address_bytes[1]
-reply_array[12] = ip_address_bytes[2]
-reply_array[13] = ip_address_bytes[3]
 
-# mac address
-reply_array[201] = wifi_mac[0]
-reply_array[202] = wifi_mac[1]
-reply_array[203] = wifi_mac[2]
-reply_array[204] = wifi_mac[3]
-reply_array[205] = wifi_mac[4]
-reply_array[206] = wifi_mac[5]
+def format_reply_array():
+    octets = ip_address_str.split('.')   
+    ip_address_bytes = bytearray(4)
+    for i in range(4):
+        ip_address_bytes[i] = int(octets[i])
+    global reply_array
+    reply_array[10] = ip_address_bytes[0]
+    reply_array[11] = ip_address_bytes[1]
+    reply_array[12] = ip_address_bytes[2]
+    reply_array[13] = ip_address_bytes[3]
+    reply_array[201] = wifi_mac[0]
+    reply_array[202] = wifi_mac[1]
+    reply_array[203] = wifi_mac[2]
+    reply_array[204] = wifi_mac[3]
+    reply_array[205] = wifi_mac[4]
+    reply_array[206] = wifi_mac[5]
 
 # Short name
 num_bytes_to_replace = len(hostname_bytes)
@@ -161,8 +151,6 @@ reply_array = modified_portion
 # Universe
 reply_array[190] = artnet_universe - 1
 
-reply = bytes(reply_array)
-
 e = espnow.ESPNow()
 peer = espnow.Peer(b'\xff\xff\xff\xff\xff\xff')
 e.peers.append(peer)
@@ -170,38 +158,39 @@ e.peers.append(peer)
 dmx_data = bytearray(513)
 
 while True:
-#    if wifi.radio.connected:
-    sock.settimeout(0.2)
-    try:
-        size, addr = sock.recvfrom_into(udp_buffer)
-        msg = bytes(udp_buffer)
-    
-        if msg[:8] == b'Art-Net\0': # Check if the received data is an Art-Net packet
+    if wifi.radio.connected:
+        sock.settimeout(0.2)
+        try:
+            size, addr = sock.recvfrom_into(udp_buffer)
+            msg = bytes(udp_buffer)
+        
+            if msg[:8] == b'Art-Net\0': # Check if the received data is an Art-Net packet
 
-            if msg[9:11] == b'\x20\x00': # is artpoll
-                print(f"artpoll packet from {addr[0]}")
-                sock.sendto(reply, (addr[0],6454) )
-                print(f"Artpoll reply sent to {addr[0]}")
-            
-            if msg[9:11] == b'\x50\x00': # is artnet DMX channel data
-                if msg[14] == (artnet_universe - 1):
-                    dmx_data[0] = wifi_channel # send wifi channel as first byte of the transmission
-                    dmx_data[1:] = msg[(17+STARTDMX):531] # send the DMX channels
-                    pixels.fill((dmx_data[2], dmx_data[1], dmx_data[3]))
-                    e.send(dmx_data[0:50], peer) # send 250 bytes max (espnow limitation)
-                    rfm9x.send(dmx_data[0:50])
-    except:
-        pass
-            
-    if time.monotonic() - last_sent_time >= 0.3:
-        e.send(dmx_data[0:50], peer) # resend last dmx values as beacon
-        rfm9x.send(dmx_data[0:50])
-        last_sent_time = time.monotonic()
-
-#    else:
-#        connect_to_wifi()
-
-
-
-
+                if msg[9:11] == b'\x20\x00': # is artpoll
+                    print(f"artpoll packet from {addr[0]}")
+                    format_reply_array()
+                    reply = bytes(reply_array)
+                    sock.sendto(reply, (addr[0],6454) )
+                    print(f"Artpoll reply sent to {addr[0]}")
+                
+                if msg[9:11] == b'\x50\x00': # is artnet DMX channel data
+                    print("dmx!")
+                    if msg[14] == (artnet_universe - 1):
+                        dmx_data[0] = wifi_channel # send wifi channel as first byte of the transmission
+                        dmx_data[1:] = msg[(17+STARTDMX):531] # send the DMX channels
+                        pixels.fill((dmx_data[2], dmx_data[1], dmx_data[3]))
+                        e.send(dmx_data[0:50], peer) # send 250 bytes max (espnow limitation)
+                        rfm9x.send(dmx_data[0:50])
+        except:
+            pass
+                
+        if time.monotonic() - last_sent_time >= 0.3:
+            e.send(dmx_data[0:50], peer) # resend last dmx values as beacon
+            rfm9x.send(dmx_data[0:50])
+            last_sent_time = time.monotonic()
+    else:
+        if not CREATE_AP:
+            connect_to_wifi()
+        else:
+            create_wifi_AP()
 
